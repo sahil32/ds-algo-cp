4720f7b16ae82f0736ee320063e507a919adff22
sahil <sahilsubnani49@gmail.com>
2021-10-17 21:50:22 +0530

    adding fiel

--- Templates/Numbers_from_string.cpp
+++ Templates/Numbers_from_string.cpp
@@ -1,5 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+void getNumbersFromString(string str) {
+    vector<int> vec;
+    stringstream ss;
+    ss<<str;
+    string temp;
+    int num ;
+    while(!ss.eof()) {
+        ss>>temp;
+        if(stringstream(temp)>>num) {
+            vec.push_back(num);
+        }
+    }
+    for(int x: vec ) {
+         cout<< x<< " ";
+    }
+}
 int main() {
-
+string str = "2 foxes and 3 dragon are divided into 69 ppls and have fun with 23 gobi";
+getNumbersFromString(str);
 }
